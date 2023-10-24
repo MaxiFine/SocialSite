8e278f62282b4868d4561cb599736fae63d30fd1
MaxFine <gibboel5@gmail.com>
2023-10-24 22:06:33 +0000

    fixed all merge conflicts

--- account/forms.py
+++ account/forms.py
@@ -22,7 +22,6 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password don\'t match.')
         return cd['password2']
-    
 
     def clean_email(self):  # used clean_fieldname method
         data = self.cleaned_data['email']
@@ -45,8 +44,6 @@ class UserEditForm(forms.ModelForm):
             return data
         
         
-
-
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
