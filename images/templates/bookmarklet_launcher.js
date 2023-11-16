4549069ade4b9912c0d74b57009acc50d3c59efe
MaxFine <gibboel5@gmail.com>
2023-11-16 09:53:15 +0000

    adding bookmarks

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -1,5 +1,6 @@
 (function(){
     if(!window.bookmarklet) {
+
     bookmarklet_js = document.body.appendChild(document.
     createElement('script'));
     bookmarklet_js.src = '//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.
@@ -7,6 +8,7 @@
     window.bookmarklet = true;
     }
     else {
+        
     bookmarkletLaunch();
     }
     })();
